2aed87d57c87a9bcff9178ba30f9b5bb7aa34c3f
Alliyana <alliyanagarcia@gmail.com>
2025-05-05 16:25:30 +0800

    Room detail header created & Initialized subfiles/body components
    Subfiles/body components are Calendar, History, Reservations for the tabs in room detail view

--- src/views/components/Layout/AdminSuperAdminLayout/SideNavLink.tsx
+++ src/views/components/Layout/AdminSuperAdminLayout/SideNavLink.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { IoIosArrowForward } from "react-icons/io";
+// import { IoIosArrowForward } from "react-icons/io";
 
 interface SideNavLinkProps {
     label: string;
