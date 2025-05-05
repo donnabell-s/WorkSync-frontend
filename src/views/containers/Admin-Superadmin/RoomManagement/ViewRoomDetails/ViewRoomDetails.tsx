5686de0c74d19429ca5455a00b7180f03f22f779
Alliyana <alliyanagarcia@gmail.com>
2025-05-05 16:25:30 +0800

    Room detail header created & Initialized subfiles/body components
    Subfiles/body components are Calendar, History, Reservations for the tabs in room detail view

--- src/views/containers/Admin-Superadmin/RoomManagement/ViewRoomDetails/ViewRoomDetails.tsx
+++ src/views/containers/Admin-Superadmin/RoomManagement/ViewRoomDetails/ViewRoomDetails.tsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { IoIosArrowBack } from "react-icons/io";
+import ViewRoomHeader from './ViewRoomHeader';
+import Calendar from './RoomDetails/Calendar';
+import History from './RoomDetails/History';
+import Reservations from './RoomDetails/Reservations';
 
 const ViewRoomDetails = () => {
+
+  const [activeTab, setActiveTab] = useState('Calendar');
+
+  const handleTabClick = (tab: string) => {
+    setActiveTab(tab);
+  };
+
   return (
-    <div>ViewRoomDetails</div>
+    <div className='flex flex-col gap-0 bg-[#F3F4F6] w-full h-full px-7 py-4 rounded-md'>
+      <span>
+        <a href="/admin/rooms/view" className='flex items-center gap-2 text-[#2563EB] text-md font-medium cursor-pointer hover:underline'>
+          <IoIosArrowBack className='size-5' />
+          Back to View Rooms
+        </a>
+      </span>
+      <div className='flex flex-col gap-4 mt-5 bg-white h-full w-full rounded-md shadow-md'>
+        <ViewRoomHeader activeTab={activeTab} handleTabClick={handleTabClick} />
+        {activeTab === 'Calendar' && <Calendar />}
+        {activeTab === 'History' && <History />}
+        {activeTab === 'Reservations' && <Reservations />}
+      </div>
+    </div>
   )
 }
 
