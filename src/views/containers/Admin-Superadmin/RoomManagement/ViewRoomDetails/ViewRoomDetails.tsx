f26fb648709171a3b4ac02fe79f1205395c0a4b2
Alliyana <alliyanagarcia@gmail.com>
2025-05-09 23:32:49 +0800

    Header responsiveness draft

--- src/views/containers/Admin-Superadmin/RoomManagement/ViewRoomDetails/ViewRoomDetails.tsx
+++ src/views/containers/Admin-Superadmin/RoomManagement/ViewRoomDetails/ViewRoomDetails.tsx
@@ -15,12 +15,12 @@ const ViewRoomDetails = () => {
 
   return (
     <div className='flex flex-col gap-0 bg-[#F3F4F6] w-full h-full px-7 py-4 rounded-md'>
-      <span>
+      <div className='w-fit'>
         <a href="/admin/rooms/view" className='flex items-center gap-2 text-[#2563EB] text-md font-medium cursor-pointer hover:underline'>
           <IoIosArrowBack className='size-5' />
           Back to View Rooms
         </a>
-      </span>
+      </div>
       <div className='flex flex-col gap-4 mt-5 bg-white h-full w-full rounded-md shadow-md'>
         <ViewRoomHeader activeTab={activeTab} handleTabClick={handleTabClick} />
         {activeTab === 'Calendar' && <Calendar />}
