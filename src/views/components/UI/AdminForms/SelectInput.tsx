8667bd1b09588f9e82f1ac56d9337a628a5bafce
Alliyana <alliyanagarcia@gmail.com>
2025-05-11 17:07:45 +0800

    Partial form functionality added

--- src/views/components/UI/AdminForms/SelectInput.tsx
+++ src/views/components/UI/AdminForms/SelectInput.tsx
@@ -3,12 +3,15 @@ import InputLabel from './InputLabel';
 
 interface SelectInputProps {
   label: string;
-//   type: 'text' | 'email' | 'password' | 'number';
+  type?: 'rooms';
   placeholder?: string;
   className?: string;
+  onClick?: (event: React.MouseEvent<HTMLButtonElement>) => void;
+  value?: string;
+  options?: string[];
 }
 
-const SelectInput: React.FC<SelectInputProps> = ({ label, placeholder, className }) => {
+const SelectInput: React.FC<SelectInputProps> = ({ label, placeholder, className, type, onClick, options }) => {
   const [filled, setFilled] = useState(false);
 
   const inputRef = useRef<HTMLSelectElement>(null);
@@ -22,14 +25,33 @@ const SelectInput: React.FC<SelectInputProps> = ({ label, placeholder, className
     }
   }, []);
 
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    if (onClick) {
+      onClick(event);
+    }
+  }
+
   return (
     <div className={`flex flex-col gap-2 ${className}`}>
       <InputLabel label={label} filled={filled} />
-      <select ref={inputRef} id='input'
-        className='w-full flex-grow text-sm border-zinc-300 border-1 rounded-md p-2 focus:outline-zinc-300 focus:outline-2 cursor-pointer'>
-        <option value="" disabled selected hidden>{placeholder || 'Select an option'}</option>
-        {/* Add options here */}
-      </select>
+      {
+        type === 'rooms' ? (
+          <button className='w-full flex-grow text-sm border-zinc-300 border-1 rounded-md p-2 focus:outline-zinc-300 focus:outline-2 cursor-pointer' onClick={handleClick}>
+            <span className='text-zinc-500'>{placeholder}</span>
+          </button>
+        ) : (
+          <select ref={inputRef} id='input'
+            className='w-full flex-grow text-sm border-zinc-300 border-1 rounded-md p-2 focus:outline-zinc-300 focus:outline-2 cursor-pointer'>
+            <option value="" disabled selected hidden>{placeholder || 'Select an option'}</option>
+            {
+              options && options.map((option, index) => (
+                <option key={index} value={option}>{option}</option>
+              ))
+            }
+          </select>
+        )
+      }
     </div>
   )
 }
