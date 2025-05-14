2bd6f244d3780a2ad194dfeaf05e5a2b4c48831e
Nicoonicss <baxalmonico03@gmail.com>
2025-05-14 21:20:01 +0800

    settings.tsx

---
+++ src/views/containers/User/Settings/settings.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AccountPreferences from "./account";
+
+const Settings: React.FC = () => {
+  return (
+    <div className="p-4">
+      <AccountPreferences />
+    </div>
+  );
+};
+
+export default Settings;
