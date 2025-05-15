14acf13748a7e05e1fe875247c829bbec0d85325
code_blooded <carigokaren14@gmail.com>
2025-05-16 07:08:54 +0800

    discarding the changes i made to the wrong file

--- src/views/containers/Admin-Superadmin/BookingManagement/ViewBookings/ViewBookings.tsx
+++ src/views/containers/Admin-Superadmin/BookingManagement/ViewBookings/ViewBookings.tsx
@@ -1,16 +1,10 @@
 import React, { useState } from 'react';
-import ViewBooking from './ViewBooking';
 
 const ViewBookings = () => {
-  const [mode, setMode] = useState<'view' | 'approved'>('view');
-
-  const handleApprove = () => {
-    setMode('approved');
-  };
+  
 
   return (
-    <div className="w-full h-full flex flex-col items-center justify-center">
-      <ViewBooking mode={mode} onApprove={handleApprove} />
+    <div>ViewBooking
     </div>
   );
 };
