2e2cf26227954d84d46cb3ba947109f7ef275b6d
donnabell-s <donnasembrano03@gmail.com>
2025-05-16 21:28:24 +0800

    edit/delete user/admin fix

--- src/views/containers/Admin-Superadmin/Users/Forms-User/EditUser.tsx
+++ src/views/containers/Admin-Superadmin/Users/Forms-User/EditUser.tsx
@@ -45,7 +45,7 @@ const EditDeleteUserForm: React.FC = () => {
   };
 
   return (
-    <div className="modal fixed inset-0 flex items-center justify-center z-50">
+    <div className="p-30 flex items-center justify-center z-50">
       <div className="modal-content bg-white p-6 rounded-lg w-full max-w-3xl shadow-lg">
         <h2 className="text-2xl font-bold mb-6">
           {mode === 'edit' ? 'EDIT USER INFORMATION' : 'Delete User'}
