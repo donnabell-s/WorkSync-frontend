2ef446c81788cc030c2ed8408b1d82beb881e887
donnabell-s <donnasembrano03@gmail.com>
2025-05-24 05:16:39 +0800

    Auto stash before merge of "user-db-implementation/donna" and "origin/user-db-implementation/donna"

--- src/views/containers/User/Forms-Room/EditBookedRoom/EditBookedRoom.tsx
+++ src/views/containers/User/Forms-Room/EditBookedRoom/EditBookedRoom.tsx
@@ -2,14 +2,18 @@ import { useEffect, useState } from 'react';
 import RoomDetailsForm from '../../../../components/Feature/RoomDetailsForm';
 import MeetingDetailsForm from '../../../../components/Feature/MeetingDetailsForm';
 import RoomBookingForm from '../../../../components/Feature/RoomBookingForm';
+import { useAuth } from '../../../../../context/AuthContext';
 
 const EditBookedRoom = () => {
-  const [roomCode, setRoomCode] = useState<string | null>(null);
+  const [bookingId, setBookingId] = useState<string | null>(null);
+  const {user} = useAuth();
 
   useEffect(() => {
+    if(user){
     const storedBookingCode = localStorage.getItem("selectedBookingId");
-    setRoomCode(storedBookingCode);
-  }, []);
+    setBookingId(storedBookingCode);
+    }
+  }, [user]);
 
   return (
     <div className='h-full bg-white flex flex-col px-4 sm:px-8 md:px-13 lg:px-25 xl:px-33 py-6 sm:py-8 md:py-10 gap-9'>
@@ -17,8 +21,8 @@ const EditBookedRoom = () => {
         <RoomBookingForm edit={true} />
       </div>
       <div  className="h-full flex flex-col md:flex-row gap-6 md:gap-10 lg:gap-13">
-        <MeetingDetailsForm roomCode={roomCode} />
-        <RoomDetailsForm roomCode={roomCode} />
+        <MeetingDetailsForm roomCode={bookingId} />
+        <RoomDetailsForm roomCode={bookingId} />
       </div>
     </div>
   );
